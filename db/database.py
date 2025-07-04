b5cb9f72468e32dfef9fec07199585bb80842a28
Dalchand Yogi <129150745+Dalchandyogi@users.noreply.github.com>
2025-07-04 10:58:33 +0530

    Update db url in database.py

--- db/database.py
+++ db/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-password = 'debug%40123' 
+# password = 'debug%40123' 
 # URL_DATABASE = f'mysql+pymysql://root:{password}@localhost:3306/customer'
-URL_DATABASE = 'mysql://root:duJTWqqqYbceDpETwCufcxirGEGHBHmg@yamabiko.proxy.rlwy.net:16263/railway'
+# URL_DATABASE = 'mysql://root:duJTWqqqYbceDpETwCufcxirGEGHBHmg@yamabiko.proxy.rlwy.net:16263/railway'
+URL_DATABASE = 'mysql+pymysql://root:duJTWqqqYbceDpETwCufcxirGEGHBHmg@yamabiko.proxy.rlwy.net:16263/railway'
+
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
