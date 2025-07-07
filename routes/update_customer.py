c5577763a99efc1298066a77a6f77072d0b68195
Dalchand Yogi <129150745+Dalchandyogi@users.noreply.github.com>
2025-07-07 12:14:46 +0530

    Updated JSONResponses for better understanding

--- routes/update_customer.py
+++ routes/update_customer.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status, HTTPException, Header
+from fastapi import APIRouter, Depends, status, Header
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session, joinedload 
 from db.database import get_db
@@ -11,15 +11,22 @@ router = APIRouter()
 
 @router.put("/update-customer", response_model=schema.CustomerResponse)
 def update_customer(
-    customer_update: schema.CustomerCreate, # Use CustomerCreate without documents
+    customer_update: schema.CustomerCreate, 
     customer_id: int = Header(..., alias="Customer-Id"),
     db: Session = Depends(get_db)
 ):
     db_customer = db.query(model_customer.Customer).filter(model_customer.Customer.id == customer_id).first()
     if not db_customer:
-        raise HTTPException(status_code=404, detail="Customer not found")
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "status_code": 404,
+                "error": "Not Found the Customer for the given ID"
+            }
+        )
 
     try:
+        
         # Update main customer fields
         for field, value in customer_update.dict(exclude={'notes'}).items(): 
             setattr(db_customer, field, value)
@@ -39,14 +46,20 @@ def update_customer(
             joinedload(model_customer.Customer.docs)
         ).filter(model_customer.Customer.id == customer_id).first()
 
-        return db_customer
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={
+                "status_code": 200,
+                "message": "Customer updated successfully",
+            }
+        )
 
     except SQLAlchemyError as e:
         db.rollback()
         return JSONResponse(
-            status_code=400,
+            status_code=status.HTTP_400_BAD_REQUEST,
             content={
-                "status": "400",
+                "status_code": 400,
                 "error": f"Failed to update customer: {str(e.__cause__)}"
             }
         )
